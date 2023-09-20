0fcfcae8d9177905839d531f465dbf70e3840aec
rudymtz7 <rmartinez@natgas.com.mx>
2023-09-20 14:44:29 -0600

    Add integration tests for services

--- src/app/services/movie/movie.service.spec.ts
+++ src/app/services/movie/movie.service.spec.ts
@@ -1,16 +1,51 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { MovieService } from './movie.service';
 
 describe('MovieService', () => {
   let service: MovieService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Import HttpClientTestingModule
+      providers: [MovieService],
+    });
+
     service = TestBed.inject(MovieService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Verify that there are no outstanding HTTP requests
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch a specific movie by episode ID successfully', inject(
+    [HttpTestingController, MovieService],
+    (httpClient: HttpTestingController, movieService: MovieService) => {
+      const episodeId = 1;
+      const dummyMovie = { title: 'Movie 1' };
+
+      // Make an HTTP GET request
+      movieService.getMovieByEpisodeId(episodeId).subscribe((movie) => {
+        expect(movie).toEqual(dummyMovie); // Check the response data
+      });
+
+      // Mock the HTTP request
+      const req = httpMock.expectOne(
+        `https://swapi.dev/api/films/${episodeId}/`
+      );
+      expect(req.request.method).toBe('GET');
+
+      // Respond with dummy data
+      req.flush(dummyMovie);
+    }
+  ));
 });
--- src/app/services/ship/ship.service.spec.ts
+++ src/app/services/ship/ship.service.spec.ts
@@ -1,16 +1,49 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ShipService } from './ship.service';
 
 describe('ShipService', () => {
   let service: ShipService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule], // Import HttpClientTestingModule
+      providers: [ShipService],
+    });
+
     service = TestBed.inject(ShipService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // Verify that there are no outstanding HTTP requests
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should fetch a specific starship by ID successfully', inject(
+    [HttpTestingController, ShipService],
+    (httpClient: HttpTestingController, shipService: ShipService) => {
+      const starshipId = 'ttps://swapi.dev/api/starships/1';
+      const dummyStarship = { name: 'Starship 1' };
+
+      // Make an HTTP GET request
+      shipService.getStarshipById(starshipId).subscribe((starship) => {
+        expect(starship).toEqual(dummyStarship); // Check the response data
+      });
+
+      // Mock the HTTP request
+      const req = httpMock.expectOne(starshipId);
+      expect(req.request.method).toBe('GET');
+
+      // Respond with dummy data
+      req.flush(dummyStarship);
+    }
+  ));
 });
